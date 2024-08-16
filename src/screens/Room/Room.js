6d481c0cdd588350fb13492c011e000b11eda365
Berk Akgül <berkak_gul51@hotmail.com>
2024-08-16 16:45:50 +0300

    send content

--- src/screens/Room/Room.js
+++ src/screens/Room/Room.js
@@ -1,9 +1,14 @@
 import {Text, View } from 'react-native'
-import React, {useLayoutEffect} from 'react'
+import React, {useLayoutEffect, useState} from 'react'
 import styles  from './styles'
+import { FloatingButton, InputModal } from '../../components';
+import database from '@react-native-firebase/database'
+import auth from '@react-native-firebase/auth'
 
 const Room = ({ route, navigation }) => {
     const { roomName } = route.params;
+    const [modalVisible, setModalVisible] = useState(false)
+    const [messageList, setMessageList] = useState([])
 
     useLayoutEffect(() => {
         // Ekran başlığını dinamik olarak ayarlayın
@@ -12,12 +17,40 @@ const Room = ({ route, navigation }) => {
         });
       }, [navigation, roomName]); // 'roomName' değiştiğinde yeniden ayarlama
 
+      const handleToggle = () => {
+        setModalVisible(!modalVisible)
+      }
+
+      function sendContent(content) {
+        const userMail = auth().currentUser.email
+          const contentObject = {
+            text: content,
+            username: userMail.split("@")[0],
+            date: new Date().toISOString(),            
+          };
+    
+          database().ref('messages/').push(contentObject)
+      }
+
+      const handleSendContent = (content) => {
+        handleToggle()
+        sendContent(content)
+      }
+
 
   return (
     <View style={styles.container}>
       <View style={styles.header_container}>
         <Text style={styles.header_text}>{roomName} odası kuruldu!</Text>
       </View>
+      
+      <FloatingButton onPress={handleToggle} />
+      <InputModal 
+        visible={modalVisible} 
+        onClose={handleToggle} 
+        onSend={handleSendContent}
+        
+      />
     </View>
   )
 }
